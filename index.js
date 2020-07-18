c5e48033f60b3fbb7859567663e58118e8574b0a
dracaster <lujanrojas.informatica@gmail.com>
2020-07-18 11:32:39 -0300

    update

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const randomstring = length => {
     let result = '';
     let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
+    for (let i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
